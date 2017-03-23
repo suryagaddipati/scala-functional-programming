e92e21f47db3a6a43c353529da52802012545b47
Meow Licious <meowlicious99@gmail.com>
2017-03-23 10:59:53 -0500

    tail:

--- List.scala
+++ List.scala
@@ -1,12 +1,33 @@
-object ListMeow extends App{
-  trait List[+A]
-  case object Nil extends List[Nothing]
-  case class Cons[+A](head: A , tail: List[A]) extends List[A]
-  val x = List(1,2,3,4,5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y case Cons(h, t) => h + sum(t)
-    case _ => 101
-  }
-  println(x)
+trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def apply[A](as: A*): List[A] = // Variadic function syntax
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+    // val x = List(1,2,3,4,5) match {
+    //   case Cons(x, Cons(2, Cons(4, _))) => x
+    //   case Nil => 42
+    //   case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y case Cons(h, t) => h 
+    //   case _ => 101
+    // }
+    // println(List(1,2,3).sum())
 }
+
+def sum(as: List[Int]): Int = as match {
+  case Nil => 0
+  case Cons(x,xs) => x + sum(xs)
+}
+
+def tail[A](as : List[A]): List[A] = as match{
+  case Nil => Nil
+  case Cons(x,Nil) => List(x)
+  case Cons(_,tail) => tail
+}
+
+println(tail(List(1)))
+
+
+
