97c00d98e0d626a7205d2e3cba17d7641d36ba23
Surya Gaddipati <sgaddipati@groupon.com>
2017-03-25 22:23:07 -0500

    size

---
+++ Tree.scala
@@ -0,0 +1,10 @@
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+def size[A](t: Tree[A]): Int = t match {
+  case Leaf(_) => 1
+  case Branch(l,r) => size(l) + size(r)
+}
+
+println(size(Branch(Leaf(1), Leaf(1))))
