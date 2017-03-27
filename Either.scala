94d27d9e71d9834945b6dffd5f64cbd37b015a69
Surya Gaddipati <sgaddipati@groupon.com>
2017-03-27 15:15:17 -0500

    map2:

--- Either.scala
+++ Either.scala
@@ -1,20 +1,22 @@
 sealed trait Either[+E, +A]{
   def map[B](f: A => B): Either[E, B] = this match{
     case Left(e) => Left(e)
-    case Right(v) = Right(f(v))
+    case Right(v) => Right(f(v))
   }
   def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match{
     case Left(e) => Left(e)
-    case Right(v) = f(v)
+    case Right(v) => f(v)
   }
   def orElse[EE >: E, AA >: A](b: => Either[EE, AA]): Either[EE, AA] = this match {
      case Left(_) => b
      case Right(a) => Right(a)
    }
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = this match {
-    case Left(e) => Left(e)
-    case Right(v) =>Right(f(a,b))
-  }
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =for { a <- this; b1 <- b } yield f(a,b1) 
+}
+
+def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = es match {
+  case  Nil => Right(Nil)
+  case h :: t => Right(Nil) 
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing] 
