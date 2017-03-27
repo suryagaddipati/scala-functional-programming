31c01e8ea74320cf4d38b5bac113ff909af7137a
Surya Gaddipati <sgaddipati@groupon.com>
2017-03-26 21:06:12 -0500

    flatmap

--- Either.scala
+++ Either.scala
@@ -3,6 +3,11 @@ sealed trait Either[+E, +A]{
     case Left(e) => Left(e)
     case Right(v) = Right(f(v))
   }
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match{
+    case Left(e) => Left(e)
+    case Right(v) = f(v)
+  }
 }
+
 case class Left[+E](value: E) extends Either[E, Nothing] 
 case class Right[+A](value: A) extends Either[Nothing, A]
