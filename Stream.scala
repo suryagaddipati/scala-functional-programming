5f071803b45a73d33c83c730d52c32b766010438
Surya Gaddipati <sgaddipati@groupon.com>
2017-03-27 21:14:37 -0500

    To List

---
+++ Stream.scala
@@ -0,0 +1,26 @@
+import Stream._
+sealed trait Stream[+A]{
+    def toList: List[A] = this match {
+      case Empty => List.empty
+      case Cons(h,t) => h() :: t().toList
+    }
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty
+    else cons(as.head, apply(as.tail: _*))
+}
+
+
+println(Stream(1,2).toList)
