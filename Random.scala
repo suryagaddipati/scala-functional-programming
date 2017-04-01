99d168e3df9a7ec7c0e6dcae43397e00d300f7a6
Surya Gaddipati <sgaddipati@groupon.com>
2017-04-01 15:55:33 -0500

    sequence

--- Random.scala
+++ Random.scala
@@ -43,3 +43,11 @@ def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =  {
   }
 }
 
+def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =  fs match {
+  case Nil => (rng => (Nil,rng))
+  case h :: t => (rng => {
+    val (headValue,nRng) = h(rng)
+    val (tailListValues, nnRng)=sequence(t)(nRng)
+    (tailListValues, nnRng)
+  })
+}
